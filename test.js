cfc45600ae7e752b6a724c5fdebc4312c922b1d2
Raymond De Castro <raymond@decastro.ph>
2023-01-14 01:48:40 +0800

    Initial commit

---
+++ test.js
@@ -0,0 +1,47 @@
+// This is the file to check whether your answer is correct or wrong.
+// Do not change anything in this file.
+
+const assert = require('assert');
+const source = require('./app');
+
+describe('[1] Count letters in a sentence.', function() {
+    const validLetter = 'o';
+    const invalidLetter = 'abc';
+    const sentence = 'The quick brown fox jumps over the lazy dog';
+
+
+});
+
+describe('[2] Check isogram.', function() {
+    const isogram = 'Machine';
+    const notIsogram = 'Hello';
+
+    
+});
+
+describe('[3] Purchase goods.', function() {
+    const price = 109.4356;
+    const discountedPrice = price * 0.8;
+    const roundedPrice = discountedPrice.toFixed(2);
+
+    
+});
+
+describe('[4] Find hot categories.', function() {
+    const items = [
+        { id: 'tltry001', name: 'soap', stocks: 14, category: 'toiletries' },
+        { id: 'tltry002', name: 'shampoo', stocks: 8, category: 'toiletries' },
+        { id: 'tltry003', name: 'tissues', stocks: 0, category: 'toiletries' },
+        { id: 'gdgt001', name: 'phone', stocks: 0, category: 'gadgets' },
+        { id: 'gdgt002', name: 'monitor', stocks: 0, category: 'gadgets' }
+    ];
+
+    
+});
+
+describe('[5] Find flying voters.', function() {
+    const candidateA = ['LIWf1l', 'V2hjZH', 'rDmZns', 'PvaRBI', 'i7Xw6C', 'NPhm2m'];
+    const candidateB = ['kcUtuu', 'LLeUTl', 'r04Zsl', '84EqYo', 'V2hjZH', 'LIWf1l'];
+
+    
+});
